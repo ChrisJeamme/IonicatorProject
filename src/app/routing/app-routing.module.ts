d7809c51082800a06653f06f9ccd0fdb82f3be40
dimitribruyere <dimitri.bruyere@outlook.fr>
2019-02-09 15:10:39 +0100

    Redirection par défaut sur page1

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const defaultsRoutes: Routes = [
-    { path: '', redirectTo: 'main', pathMatch: 'full' },
-    {
-        path: 'main',
-        loadChildren: '../modules/main/main.page.module#MainPageModule'
-    },
+    { path: '', redirectTo: 'page1', pathMatch: 'full' },
     {
         path: 'page1',
         loadChildren: '../modules/pages/page1/page1.page.module#Page1PageModule'
